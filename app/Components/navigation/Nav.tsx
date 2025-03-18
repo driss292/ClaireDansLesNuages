c22a14ed96f09546f0ff55f7e3b73b9c4959bd14
driss292 <drisskaci@outlook.fr>
2025-03-18 13:20:41 +0100

    Debug component

--- app/Components/navigation/Nav.tsx
+++ app/Components/navigation/Nav.tsx
@@ -1,9 +1,11 @@
 import { navigationData } from "@/lib/data";
 import NavItem from "./NavItem";
+import DebugTailwind from "../Debug/debugTailwind";
 
 export default function Nav() {
   return (
     <nav className="max-w-7xl mx-auto h-11 flex justify-evenly items-center bg-white px-10">
+      <DebugTailwind />
       {navigationData.map((item) => (
         <NavItem key={item.name} item={item} />
       ))}
