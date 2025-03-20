54a578c3b1d5f0d0886993bd4d06a31565424481
driss292 <drisskaci@outlook.fr>
2025-03-20 11:41:39 +0100

    start design contact page with form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,58 @@
+import Image from "next/image";
+
 export default function ContactPage() {
   return (
-    <main>
-      <h1>Contact</h1>
-    </main>
+    <section className="max-w-7xl mx-auto h-full bg-white px-10 pb-10 pt-1 flex flex-col items-center justify-center">
+      <div className="max-w-[90%] sm:w-[475px]  flex flex-col items-center justify-center">
+        <div className="pt-7 w-full flex-1 flex items-center justify-center">
+          <div className="w-[150px] h-[150px] md:w-[250px] md:h-[250px] sm:w-[240px] sm:h-[240px]">
+            <Image
+              width={280}
+              height={280}
+              src="/assets/profil-2.jpg"
+              alt="photo profil"
+              className="w-full h-full object-cover border-[5px] border-[#686868] rounded-full"
+            />
+          </div>
+        </div>
+        <div className="w-full flex-1 p-0 sm:px-4">
+          <h2 className="text-xl sm:text-3xl text-center sm:mb-3 font-copperplateRegular">
+            Où me Contacter ?
+          </h2>
+          <p className="text-center font-copperplateLight text-[10px] sm:text-xs md:text-sm lg:text-lg">
+            clairedanslesnuages@hotmail.com
+          </p>
+          <p className="text-center mb-5 sm:mb-4 text-sm font-copperplateLight sm:text-base">
+            06.88.71.64.83
+          </p>
+          <form className="pb-0">
+            <input
+              type="text"
+              placeholder="Nom *"
+              className="w-full h-[30px] bg-[var(--formBG)] rounded-sm mt-2 px-2 text-sm sm:text-base"
+            />
+            <input
+              type="text"
+              placeholder="Email *"
+              className="w-full h-[30px] bg-[var(--formBG)] rounded-sm mt-2 px-2 text-sm sm:text-base"
+            />
+            <input
+              type="text"
+              placeholder="Sujet"
+              className="w-full h-[30px] bg-[var(--formBG)] rounded-sm mt-2 px-2 text-sm sm:text-base"
+            />
+            <textarea
+              placeholder="Message"
+              className="w-full h-32 bg-[var(--formBG)] rounded-sm mt-2 px-2 pt-2 text-sm sm:text-base resize-none"
+            ></textarea>
+            <div className="flex justify-end mt-1">
+              <button className="w-20 h-7 flex justify-center items-center bg-[var(--formText)] p-1 text-xs text-white">
+                Envoyer
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
   );
 }
