ce84449426490d534b917e6db7477079fecf8ac4
driss292 <drisskaci@outlook.fr>
2025-03-20 10:58:39 +0100

    test commit

--- app/inside/page.tsx
+++ app/inside/page.tsx
@@ -86,6 +86,11 @@ export default function InsidePage() {
           architecto sunt, ipsaendi dolore sint beatae blanditiis placeat
           recusandae aliquam, sapiente, aut consequatur earum!
         </p>
+        <p>
+          Incidunt similique accusantium culpa facilis quasi labore? Odio
+          architecto sunt, ipsaendi dolore sint beatae blanditiis placeat
+          recusandae aliquam, sapiente, aut consequatur earum!
+        </p>
       </div>
     </section>
   );
