29d6617a20bc4edf6513739f7ca5d2f5fd3fda39
driss292 <drisskaci@outlook.fr>
2025-07-04 15:16:50 +0200

    feat - add grid template for imags

--- app/space/page.tsx
+++ app/space/page.tsx
@@ -7,15 +7,21 @@ export default function InsidePage() {
       {insidePageData.map((item) => (
         <div key={item.id} className="w-full h-auto">
           <h2>{item.title.toLocaleUpperCase()}</h2>
-          <div>
+
+          <div className="grid grid-cols-2 gap-4 mt-4">
             {item.images.map((image, index) => (
-              <Image
-                width={300}
-                height={300}
-                src={image}
-                alt="image"
+              <div
                 key={`${item.id}-${index}`}
-              />
+                className={`relative ${index === 2 ? "col-span-2" : ""}`}
+              >
+                <Image
+                  width={600}
+                  height={400}
+                  src={image}
+                  alt="image"
+                  className="w-full h-auto"
+                />
+              </div>
             ))}
           </div>
         </div>
